dd45090edae7d78bd0f85d7ae06e483b47731f82
satisumit <sumit.sati2508@gmail.com>
2024-08-18 16:06:11 +0530

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
-const Header = (props: Props) => {
+const Header = async (props: Props) => {
+  var isLoggedIn;
+  if (1) {
+    isLoggedIn = true;
+  } else {
+    isLoggedIn = false;
+  }
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <nav className="flex justify-start items-center px-5 py-2 text-xl gap-40 bg-gradient-to-br from-blue-700 to bg-teal-300 h-24 font-ubuntu-font">
+        <a
+          href="#"
+          className="logo flex justify-between items-center gap-2 cursor-pointer"
+        >
+          <img src="https://via.placeholder.com/60" alt="logo" />
+          <h1 className="text-3xl">HealthSync</h1>
+        </a>
+        <div className="nav-links flex justify-between items-center gap-12 text-slate-950">
+          <a
+            href="#"
+            className="hover:text-orange-900 hover:scale-110 transition-transform duration-300"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="hover:text-orange-900 hover:scale-110 transition-transform duration-300"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="hover:text-orange-900 hover:scale-110 transition-transform duration-300"
+          >
+            Contact Us
+          </a>
+          {isLoggedIn ? (
+            <>
+              <a
+                href="#"
+                className="hover:text-orange-900 hover:scale-110 transition-transform duration-300"
+              >
+                Dashboard
+              </a>
+              <a
+                href="#"
+                className="hover:text-orange-900 hover:scale-110 transition-transform duration-300"
+              >
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <a
+                href="#"
+                className="hover:text-orange-900 hover:scale-110 transition-transform duration-300"
+              >
+                Login
+              </a>
+              <a
+                href="#"
+                className="hover:text-orange-900 hover:scale-110 transition-transform duration-300"
+              >
+                Register
+              </a>
+            </>
+          )}
+          <a
+            href="#"
+            className="hover:text-orange-900 hover:scale-110 transition-transform duration-300"
+          >
+            Complaints
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
