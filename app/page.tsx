1b0ca01ce29dfdb07b5f87350136701ee34f5d9e
satisumit <sumit.sati2508@gmail.com>
2024-08-18 14:20:51 +0530

    ''Dew to Dewansh

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,8 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel and Mountain Dew.
+            Instantly deploy your Next.js site to a shareable URL with Vercel
+            and Mountain Dewansh.
           </p>
         </a>
       </div>
