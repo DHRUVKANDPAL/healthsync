2733d523b84784ce32f8af4e9f1930c024dfb70a
satisumit <sumit.sati2508@gmail.com>
2024-08-19 12:02:12 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   webpack: (config) => {
-      config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
-      return config;
-    },
+  webpack: (config) => {
+    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+    return config;
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
 };
 
 export default nextConfig;
