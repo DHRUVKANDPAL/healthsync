c0106feb5ffc74e7a1358eeb4329a867e57b51e6
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-08-20 00:07:51 +0530

    tab switch min h fix

--- app/patient-auth/page.tsx
+++ app/patient-auth/page.tsx
@@ -8,7 +8,7 @@ type Props = {};
 const page = (props: Props) => {
   return (
     // <></>
-    <div className="min-h-screen w-full max-w-screen-2xl flex justify-center py-10 mx-auto">
+    <div className="min-h-[500px] w-full max-w-screen-2xl flex justify-center py-10 mx-auto">
       <Tabswitcher
         signUpTab={<Signup></Signup>}
         signinTab={<Signin></Signin>}
