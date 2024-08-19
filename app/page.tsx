e202f6eb822cce1b0d1d022b4593358038fd91cd
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-08-19 11:14:09 +0530

    testimonials adding in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import Testimonial from "@/components/Testimonial";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Hero></Hero>
+      <Testimonial></Testimonial>
     </main>
   );
 }
