ff341c716d8f05ac6a7e2a3bedda576f650c7e5c
satisumit <sumit.sati2508@gmail.com>
2024-08-19 17:39:23 +0530

    updated navbar for mobile screens

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,18 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
-type Props = {};
+type Props = {
+  props?: string;
+};
 
 const Logo = (props: Props) => {
   return (
-    <h1 className="text-3xl sm:text-4xl font-poppins-font font-semibold text-teal-500 text-center sm:text-left">
+    <h1
+      className={cn(
+        "text-3xl sm:text-4xl font-poppins-font font-semibold text-teal-500 text-center sm:text-left ",
+        props
+      )}
+    >
       Health<span className="text-teal-700 font-bold">Sync</span>
     </h1>
   );
