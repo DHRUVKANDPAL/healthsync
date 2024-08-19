1234fd2d3544c054c99f19fba8f62c908f9b01a4
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-08-19 09:40:40 +0530

    toaster richcolors added

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header></Header>
         {children}
-        <Toaster></Toaster>
+        <Toaster richColors></Toaster>
       </body>
     </html>
   );
