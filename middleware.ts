9e7d299d5544fcc86ba25be3494db2ead8d6c321
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-08-19 12:15:49 +0530

    middleware

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/patient-dash")) {
+    let isLoggedin=request.cookies.get("user-auth-cookie")
+    if(isLoggedin){
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/patient-auth",request.url))
+  }
+}
