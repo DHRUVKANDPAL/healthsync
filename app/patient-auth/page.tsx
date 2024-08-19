53501ad276f4b5cda082d22afaa8e1f26abed6ac
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-08-19 07:35:24 +0530

    signup complete

--- app/patient-auth/page.tsx
+++ app/patient-auth/page.tsx
@@ -7,7 +7,8 @@ type Props = {};
 
 const page = (props: Props) => {
   return (
-    <div className="min-h-screen flex justify-center items-center mt-10">
+    // <></>
+    <div className="min-h-screen max-w-full flex justify-center pt-10">
       <Tabswitcher
         signUpTab={<Signup></Signup>}
         signinTab={<Signin></Signin>}
--- components/Signup.tsx
+++ components/Signup.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -25,55 +25,62 @@ import { z } from "zod";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import { signup } from "@/app/patient-auth/auth.actions";
-export const SignUpSchema = z.object({
-  email: z.string().email("This is not a valid email"),
-  username: z.string().min(2).max(50),
-  gender:z.string(),
-  dob:z.string().datetime(),
-  aadharno:z.string(),
-  bloodgroup:z.string(),
-  contactno:z.string(),
-  alternatecontactno:z.string(),
-  address:z.string(),
-  emregencycontact:z.string(),
-  prevHis:z.string(),
-  password: z.string().min(6),
-  confirmPassword: z.string().min(6),
-}).refine(data => data.password === data.confirmPassword, {
-   message: 'Passwords do not match',
-   path: ['confirmPassword']
-});
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+export const SignUpSchema = z
+  .object({
+    email: z.string().email("This is not a valid email"),
+    username: z.string().min(2).max(50),
+    gender: z.string(),
+    dob: z.string(),
+    aadharno: z.string(),
+    bloodgroup: z.string(),
+    contactno: z.string(),
+    alternatecontactno: z.string(),
+    address: z.string(),
+    emregencycontact: z.string(),
+    prevHis: z.string(),
+    password: z.string().min(6),
+    confirmPassword: z.string().min(6),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 const Signup = () => {
-   const router=useRouter();
+  const router = useRouter();
   const form = useForm<z.infer<typeof SignUpSchema>>({
     resolver: zodResolver(SignUpSchema),
     defaultValues: {
-      email:"nil@gmail.com",
+      email: "",
       username: "",
-      gender:"",
-      aadharno:"",
-      dob:"",
-      bloodgroup:"",
-      contactno:"",
-      alternatecontactno:"",
-      prevHis:"",
-      password:"",
-      confirmPassword:"",
+      gender: "",
+      aadharno: "",
+      dob: "",
+      bloodgroup: "",
+      contactno: "",
+      alternatecontactno: "",
+      prevHis: "",
+      password: "",
+      confirmPassword: "",
     },
   });
   async function onSubmit(values: z.infer<typeof SignUpSchema>) {
-      const res=await signup(values);
-      if(res.success){
-         toast.success('Account created successfully')
-         router.push('/patient-dash')
-      }
-      else{
-         toast.error(res.error)
-      }
-    
+    const res = await signup(values);
+    if (res.success) {
+      toast.success("Account created successfully");
+      router.push("/patient-dash");
+    } else {
+      toast.error(res.error);
+    }
   }
   return (
-    <Card className="w-[300px] sm:w-[430px] md:w-[540px]">
+    <Card className="w-[300px] sm:w-[430px] md:w-[580px]">
       <CardHeader>
         <CardTitle>Sign Up</CardTitle>
         <CardDescription>Sign up here.</CardDescription>
@@ -101,21 +108,37 @@ const Signup = () => {
                 <FormItem>
                   <FormLabel>DOB</FormLabel>
                   <FormControl>
-                    <Input placeholder="shadcn" type="datetime" {...field} />
+                    <Input
+                      placeholder="25 Aug 2004"
+                      type="datetime"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
-             <FormField
+            <FormField
               control={form.control}
               name="gender"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Gender</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Male" type="text" {...field} />
-                  </FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a verified Gender" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="Male">Male</SelectItem>
+                      <SelectItem value="Female">Female</SelectItem>
+                      <SelectItem value="Others">Others</SelectItem>
+                    </SelectContent>
+                  </Select>
                   <FormMessage />
                 </FormItem>
               )}
@@ -126,9 +149,26 @@ const Signup = () => {
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Blood Group</FormLabel>
-                  <FormControl>
-                    <Input placeholder="B-" type="text" {...field} />
-                  </FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a verified Blood Group" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="A+">A+</SelectItem>
+                      <SelectItem value="A-">A-</SelectItem>
+                      <SelectItem value="B+">B+</SelectItem>
+                      <SelectItem value="B-">B-</SelectItem>
+                      <SelectItem value="O+">O+</SelectItem>
+                      <SelectItem value="O-">O-</SelectItem>
+                      <SelectItem value="AB+">AB+</SelectItem>
+                      <SelectItem value="AB-">AB-</SelectItem>
+                    </SelectContent>
+                  </Select>
                   <FormMessage />
                 </FormItem>
               )}
@@ -140,7 +180,7 @@ const Signup = () => {
                 <FormItem>
                   <FormLabel>Contact No</FormLabel>
                   <FormControl>
-                    <Input placeholder="8756680899" type="text" {...field} />
+                    <Input placeholder="9999988888" type="text" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -153,7 +193,7 @@ const Signup = () => {
                 <FormItem>
                   <FormLabel>Alternate Contact No</FormLabel>
                   <FormControl>
-                    <Input placeholder="9999888877" type="text" {...field} />
+                    <Input placeholder="7777755555" type="text" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -166,7 +206,11 @@ const Signup = () => {
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input placeholder="suraj@gmail.com" type="email" {...field} />
+                    <Input
+                      placeholder="suraj@gmail.com"
+                      type="email"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -185,7 +229,7 @@ const Signup = () => {
                 </FormItem>
               )}
             />
-            
+
             <FormField
               control={form.control}
               name="password"
