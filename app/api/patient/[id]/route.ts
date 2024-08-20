42e5279ce25725d9ac12d9d6873d4e27529c5ac3
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-08-20 11:42:59 +0530

    id check

--- app/api/patient/[id]/route.ts
+++ app/api/patient/[id]/route.ts
@@ -3,6 +3,14 @@ import { getUser } from "@/lib/lucia";
 export async function GET(req:Request,{params}:{params:{id:string}}){
   const id=params.id
   let user=await getUser();
+  if(user && id!==user.id){
+    return new Response(
+      JSON.stringify({ success: false }),
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
   if(user){
     return new Response(
       JSON.stringify({ success: true }),
