c899db56cd7ec9d0e71ef15b60ee8a66e1570ebd
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-08-28 11:15:00 +0530

    room form created

--- app/hospital-dash/[id]/rooms/page.tsx
+++ app/hospital-dash/[id]/rooms/page.tsx
@@ -1,9 +1,113 @@
-import React from 'react'
+"use client";
 
-const Rooms = () => {
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const formSchema = z.object({
+  roomno: z.string().min(1),
+  typeof: z.string(),
+});
+
+export default function Rooms() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      roomno: "",
+      typeof: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div>Rooms</div>
-  )
+    <div className="min-h-[500px] w-full max-w-screen-2xl flex justify-center py-10 mx-auto bg-gradient-to-r dark:from-slate-950 dark:via-slate-900 dark:to-slate-950">
+      <Card className="w-[300px] sm:w-[430px] md:w-[540px]">
+        <CardHeader>
+          <CardTitle>
+            <span>Create Room</span>{" "}
+          </CardTitle>
+          <CardDescription>Create room here.</CardDescription>
+        </CardHeader>
+        <CardContent className="">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="roomno"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Room no</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display room name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="typeof"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Type of Room</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a verified type to display" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="ICU">ICU</SelectItem>
+                        <SelectItem value="Single Room">Single Room</SelectItem>
+                        <SelectItem value="General Ward">
+                          General Ward
+                        </SelectItem>
+                        <SelectItem value="OPD">OPD</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
-
-export default Rooms
