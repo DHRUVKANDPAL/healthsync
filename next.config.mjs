314ed23d7164fddebf9d859407608fceac861307
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-10-19 15:51:51 +0530

    remote pattern allowed

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,12 @@ const nextConfig = {
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
     return config;
   },
-  images: {
-    domains: ["res.cloudinary.com", "i.imghippo.com", "via.placeholder.com","files.edgestore.dev"],
-  },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "**",
+    },
+  ],
 };
 
 export default nextConfig;
