b55d1610ff1504b7bae4515aede5c975a3523e40
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-10-19 17:52:28 +0530

    more SEO Optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,22 +8,23 @@ import Footer from "@/components/Footer";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 import { usePathname } from "next/navigation";
 import { headers } from "next/headers";
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-  title: "HealthSync | Advanced Real-Time Healthcare Solution for Hospitals, Patients, Doctors, and Pharmacies",
-  description: "HealthSync is an all-in-one healthcare platform designed for hospitals, patients, doctors, and pharmacies. Offering real-time OPD management, bed availability tracking, patient care optimization, pharmacy inventory management, and a centralized health dashboard. Experience seamless healthcare collaboration with cutting-edge technology built for the modern healthcare ecosystem.",
+  title:
+    "HealthSync | Advanced Real-Time Healthcare Solution for Hospitals, Patients, Doctors, and Pharmacies",
+  description:
+    "HealthSync is an all-in-one healthcare platform designed for hospitals, patients, doctors, and pharmacies. Offering real-time OPD management, bed availability tracking, patient care optimization, pharmacy inventory management, and a centralized health dashboard. Experience seamless healthcare collaboration with cutting-edge technology built for the modern healthcare ecosystem.",
   icons: {
-    icon: [
-      "https://i.imghippo.com/files/4OFtW1729338402.jpg",
-    ],
+    icon: ["https://i.imghippo.com/files/4OFtW1729338402.jpg"],
   },
   openGraph: {
-    title: "HealthSync | Real-Time Healthcare Solution for Hospitals, Doctors, Patients & Pharmacies",
-    description: "HealthSync revolutionizes healthcare with an integrated platform for hospitals, patients, doctors, and pharmacies. It offers real-time OPD tracking, pharmacy inventory control, bed availability updates, and a centralized patient dashboard for optimized care.",
+    title:
+      "HealthSync | Real-Time Healthcare Solution for Hospitals, Doctors, Patients & Pharmacies",
+    description:
+      "HealthSync revolutionizes healthcare with an integrated platform for hospitals, patients, doctors, and pharmacies. It offers real-time OPD tracking, pharmacy inventory control, bed availability updates, and a centralized patient dashboard for optimized care.",
     url: "https://healthsync-alpha.vercel.app/",
     images: [
       {
@@ -36,14 +37,16 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "HealthSync | Transforming Real-Time Healthcare for Hospitals, Doctors, Patients & Pharmacies",
-    description: "Join the future of healthcare with HealthSync's all-in-one platform for hospitals, patients, doctors, and pharmacies. Real-time OPD management, bed tracking, pharmacy solutions, and patient dashboards for seamless healthcare collaboration.",
+    title:
+      "HealthSync | Transforming Real-Time Healthcare for Hospitals, Doctors, Patients & Pharmacies",
+    description:
+      "Join the future of healthcare with HealthSync's all-in-one platform for hospitals, patients, doctors, and pharmacies. Real-time OPD management, bed tracking, pharmacy solutions, and patient dashboards for seamless healthcare collaboration.",
     images: ["https://i.imghippo.com/files/4OFtW1729338402.jpg"],
   },
-  keywords: "HealthSync, healthcare platform, real-time healthcare solution, hospital management software, patient care system, pharmacy inventory management, doctor-patient communication, OPD tracking software, bed availability tracker, healthcare technology, health dashboard, modern healthcare ecosystem,OPD queuing, patient history management, patient care optimization",
+  keywords:
+    "HealthSync, healthcare platform, real-time healthcare solution, hospital management software, patient care system, pharmacy inventory management, doctor-patient communication, OPD tracking software, bed availability tracker, healthcare technology, health dashboard, modern healthcare ecosystem,OPD queuing, patient history management, patient care optimization",
   robots: "index, follow",
 };
-
 
 export default function RootLayout({
   children,
@@ -60,22 +63,51 @@ export default function RootLayout({
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
-        <link rel="shortcut icon" href="https://i.imghippo.com/files/4OFtW1729338402.jpg" />
-        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
-          "@context": "https://schema.org",
-          "@type": "WebApplication",
-          "name": "HealthSync",
-          "description": "HealthSync is an all-in-one healthcare platform for hospitals, doctors, patients, and pharmacies, offering real-time OPD management, bed tracking, pharmacy control, and patient dashboards.",
-          "applicationCategory": "Healthcare",
-          "operatingSystem": "Web",
-          "author": {
-            "@type": "Organization",
-            "name": "HealthSync"
-          },
-          "url": "https://healthsync-alpha.vercel.app/",
-          "image": "https://i.imghippo.com/files/4OFtW1729338402.jpg",
-          "screenshot": "https://i.imghippo.com/files/4OFtW1729338402.jpg",
-        })}} />
+        <link
+          rel="shortcut icon"
+          href="https://i.imghippo.com/files/4OFtW1729338402.jpg"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebApplication",
+              name: "HealthSync",
+              description:
+                "HealthSync is an all-in-one healthcare platform for hospitals, doctors, patients, and pharmacies, offering real-time OPD management, bed tracking, pharmacy control, and patient dashboards.",
+              applicationCategory: "Healthcare",
+              operatingSystem: "Web",
+              author: [
+                {
+                  "@type": "Organization",
+                  name: "HealthSync",
+                },
+                {
+                  "@type": "Person",
+                  name: "Dhruv Kandpal",
+                  role: "Team Lead & Backend Developer",
+                },
+                {
+                  "@type": "Person",
+                  name: "Sumit Sati",
+                  role: "Frontend Developer",
+                },
+                {
+                  "@type": "Person",
+                  name: "Dewansh Mishra",
+                },
+                {
+                  "@type": "Person",
+                  name: "Tarush Gupta",
+                },
+              ],
+              url: "https://healthsync-alpha.vercel.app/",
+              image: "https://i.imghippo.com/files/4OFtW1729338402.jpg",
+              screenshot: "https://i.imghippo.com/files/4OFtW1729338402.jpg",
+            }),
+          }}
+        />
         <link rel="canonical" href="https://healthsync-alpha.vercel.app/" />
       </head>
       <body className={inter.className}>
