35567d8268f8ecf93dae1ab9fc60eb7bedf16410
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2025-02-01 17:07:08 +0530

    doctor dash

--- app/doctor-dash/[id]/page.tsx
+++ app/doctor-dash/[id]/page.tsx
@@ -38,7 +38,7 @@ export default function Page() {
       }
     };
     fetchDoctorData();
-  });
+  },[]);
   if (doctorData === undefined || doctorData === null) {
     return <DoctorProfileShimmer/>;
     ;
